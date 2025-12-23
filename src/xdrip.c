5fc725bac0ef34c48eacc203b84c04ae288574e1
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2025-12-23 09:25:35 +0100

    Adjust time ago placement

--- src/xdrip.c
+++ src/xdrip.c
@@ -2217,8 +2217,8 @@ void window_load_cgm(Window *window_cgm) {
 #endif
     //cgmtime_layer = text_layer_create(GRect(5, 58, 40, 24));
 #ifdef PBL_BW
-    //
-    cgmtime_layer = text_layer_create(GRect(104, 40, 40, 24));
+    // To the left, just beneath glucose value
+    cgmtime_layer = text_layer_create(GRect(4, 30, 40, 24));
 #elif PBL_ROUND
     cgmtime_layer = text_layer_create(GRect(5, 58, 40, 24));
 #else
@@ -2233,8 +2233,7 @@ void window_load_cgm(Window *window_cgm) {
     text_layer_set_background_color(cgmtime_layer, GColorClear);
 #endif
     text_layer_set_font(cgmtime_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-    //text_layer_set_text_alignment(cgmtime_layer, GTextAlignmentLeft);
-    text_layer_set_text_alignment(cgmtime_layer, GTextAlignmentCenter);
+    text_layer_set_text_alignment(cgmtime_layer, GTextAlignmentLeft);
     layer_add_child(window_layer_cgm, text_layer_get_layer(cgmtime_layer));
 
 #ifdef PBL_BW
