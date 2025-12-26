82b728088f133a740d045d4a141c3458158c8600
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2025-12-26 13:07:27 +0100

    Move TEST_MODE code to header

---
+++ src/config.h
@@ -0,0 +1,22 @@
+// TEST_MODE: Display test data instead of real data
+//
+// #define TEST_MODE
+// #define TEST_SHOW_GRAPH
+// #define TEST_SHOW_DELTA
+// #define TEST_SHOW_DELTA_UNITS
+// #define TEST_SHOW_SLOPE_ARROWS
+// #define TEST_SHOW_IOB
+const int test_mode_minutes_ago = 59; // "59" takes most space, good for testing
+
+// DEBUG_OUTLINE: Display layer outlines for debugging
+//
+// #define DEBUG_OUTLINE
+// #define DEBUG_OUTLINE_BG         // Blood glucose value
+// #define DEBUG_OUTLINE_ARROW      // Trend arrow
+// #define DEBUG_OUTLINE_DELTA      // BG delta/change
+// #define DEBUG_OUTLINE_IOB        // Insulin on board
+// #define DEBUG_OUTLINE_GRAPH      // Trend graph
+// #define DEBUG_OUTLINE_TIMEAGO    // Time ago
+// #define DEBUG_OUTLINE_TIME       // Current time
+// #define DEBUG_OUTLINE_DATE       // Date
+// #define DEBUG_OUTLINE_BATTERY    // Battery layers (currently hidden)
--- src/xdrip.c
+++ src/xdrip.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+#include "config.h"
 
 // IDE DEBUG ONLY
 //#define PBL_HEALTH
@@ -7,27 +8,6 @@
 Make sure you set this to 0 before building a release. */
 
 //#define DEBUG_LEVEL 1
-
-// TEST_MODE: Display test data instead of real data
-// #define TEST_MODE
-// #define TEST_SHOW_GRAPH
-// #define TEST_SHOW_DELTA
-// #define TEST_SHOW_DELTA_UNITS
-// #define TEST_SHOW_ARROWS
-// #define TEST_SHOW_IOB
-const int test_mode_minutes_ago = 59; // "59" takes most space, good for testing
-
-// DEBUG_OUTLINE: Display layer outlines for debugging
-// #define DEBUG_OUTLINE
-// #define DEBUG_OUTLINE_BG         // Blood glucose value
-// #define DEBUG_OUTLINE_ARROW      // Trend arrow
-// #define DEBUG_OUTLINE_DELTA      // BG delta/change
-// #define DEBUG_OUTLINE_IOB        // Insulin on board
-// #define DEBUG_OUTLINE_GRAPH      // Trend graph
-// #define DEBUG_OUTLINE_TIMEAGO    // Time ago
-// #define DEBUG_OUTLINE_TIME       // Current time
-// #define DEBUG_OUTLINE_DATE       // Date
-// #define DEBUG_OUTLINE_BATTERY    // Battery layers (currently hidden)
 
 // Layout constants
 #define EDGE_MARGIN 4
